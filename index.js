e2760fc5d10a512e7aecb164261e6a7b1288321f
Jake <jakerfilut@gmail.com>
2021-11-03 16:20:29 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -11,6 +11,43 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+let titleCased = () => {
+
+return tutorials.map(tutorials => {
+  let splitTutorials = tutorials.split(" ")
+  console.log(splitTutorials)
+  for(let i = 0; i < splitTutorials.length; i++){
+  splitTutorials[i] = splitTutorials[i][0].toUpperCase() 
+        + splitTutorials[i].substring(1);
+        console.log(splitTutorials)
 }
+  return splitTutorials.join(" ")})
+  console.log(splitTutorials)
+}
+
+
+
+//console.log(tutorials)
+//console.log(splitTutorials)
+  
+  
+//return each word with a capital letter in the front splitTutorial[0].toUpperCase() + splitTutorial[0].substr(1);
+
+
+//let test = splitTutorials.join(' ');
+
+
+
+
+
+  // const capitalTutorial = splitTutorials.map(word => { return word[0].toUpperCase() +word.substr(1);
+//  splitTutorials.map((word) => word[0].toUpperCase() + word.substring(1))
+//  . join('')
+// console.log(capitalTutorial)
+  // })
+   
+   
+    // join back together .join(" ")
+
+
+   
